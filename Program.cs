313e48c7d2aa407dffefa69b6d8b3b9281f3cdf9
Gasan Nuriev <C:\Users\Gasan\AppData\Roaming\The Bat!>
2023-02-15 13:10:36 +0300

    new commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является 
+// ли число чётным (делится ли оно на два без остатка).
+
+
+Console.Write("Введите число  ");      // вводим начальную информацию 
+
+string inputValue = Console.ReadLine();     // для первых чисел присваиваем переменных из консоли
+
+ int Value = Convert.ToInt32(inputValue);
+ 
+ 
+ if (Value % Value == 1)  
+{
+    System.Console.WriteLine($"Число {Value} четное число ");
+}
+else
+{
+    System.Console.WriteLine ($"Число {Value} нечетное число");
+}
